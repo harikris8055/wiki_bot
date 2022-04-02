27d1a13355a1947a72db2ddbbf0747bbb60b0b7e
hari-krishnan-sevenchats <hari.krishnan@sevenchats.com>
2022-04-02 16:25:28 +0530

    added .env file

--- index.js
+++ index.js
@@ -4,14 +4,14 @@
 
 const TelegramBot = require('node-telegram-bot-api')
 const wiki = require('./wiki')
+const dotenv = require('dotenv')
 
-
+dotenv.config();
 //main 
 function run() {
 
-
     //your newely created bot token here
-    const bot = new TelegramBot('5131516016:AAHup8PcEj2ERwLdVc0xqjrtPn3_r4IZ-zE', { polling: true })
+    const bot = new TelegramBot(process.env.TOKEN, { polling: true })
 
 
     bot.on('message', async (msg) => {
